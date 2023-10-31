7587195bbb6bac552c10c2236c58952055937011
Hamza Tahir <htahir111@gmail.com>
2023-10-31 23:59:47 +0100

    Deploy

--- huggingface_sagemaker_deployer/steps/deploying/sagemaker_deployment.py
+++ huggingface_sagemaker_deployer/steps/deploying/sagemaker_deployment.py
@@ -18,6 +18,13 @@
 from typing_extensions import Annotated
 from zenml import step, get_step_context
 from zenml.logger import get_logger
+from sagemaker.huggingface import get_huggingface_llm_image_uri
+from sagemaker.huggingface import HuggingFaceModel
+import sagemaker
+import boto3
+
+import os
+import time
 
 # Initialize logger
 logger = get_logger(__name__)
@@ -33,6 +40,50 @@ def deploy_hf_to_sagemaker() -> Annotated[str, "sagemaker_endpoint_name"]:
     """
     context = get_step_context()
     mv = context.model_config.get_or_create_model_version()
-    print(mv.get_artifact_object(name="huggingface_url").metadata["repo_id"].value)
-    print(mv.get_artifact_object(name="huggingface_url").metadata["revision"].value)
-    return ""
+    repo_id = mv.get_artifact_object(name="huggingface_url").metadata["repo_id"].value
+    revision = mv.get_artifact_object(name="huggingface_url").metadata["revision"].value
+
+    REGION_NAME = "us-east-1"
+    os.environ["AWS_DEFAULT_REGION"] = REGION_NAME
+    ROLE_NAME = "hamza_connector"
+
+    auth_arguments = {
+        "aws_access_key_id": os.environ["AWS_ACCESS_KEY_ID"],
+        "aws_secret_access_key": os.environ["AWS_SECRET_ACCESS_KEY"],
+        "aws_session_token": os.environ["AWS_SESSION_TOKEN"],
+        "region_name": REGION_NAME,
+    }
+
+    iam = boto3.client("iam", **auth_arguments)
+    role = iam.get_role(RoleName=ROLE_NAME)["Role"]["Arn"]
+
+    session = sagemaker.Session(boto3.Session(**auth_arguments))
+
+    # image uri
+    llm_image = get_huggingface_llm_image_uri("huggingface")
+
+    # Falcon 7b
+    hub = {"HF_MODEL_ID": repo_id, "HF_MODEL_REVISION": revision}
+
+    # Hugging Face Model Class
+    huggingface_model = HuggingFaceModel(
+        env=hub,
+        role=role,  # iam role from AWS
+        image_uri=llm_image,
+        sagemaker_session=session,
+    )
+
+    # deploy model to SageMaker
+    predictor = huggingface_model.deploy(
+        initial_instance_count=1,  # number of instances
+        instance_type="ml.g5.2xlarge",  #'ml.g5.4xlarge'
+        container_startup_health_check_timeout=300,
+    )
+    endpoint_name = predictor.endpoint_name
+
+    time.sleep(10)
+
+    # DELETE ENDPOINT to avoid unnecessary expenses
+    predictor.delete_model()
+    predictor.delete_endpoint()
+    return endpoint_name
